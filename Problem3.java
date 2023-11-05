a6cbe49ab5b623075316b855f33d59be4f01745d
Sam948-byte <techguy763@gmail.com>
2023-11-04 23:17:58 -0500

    mark three as todo

--- Problem3.java
+++ Problem3.java
@@ -1,5 +1,5 @@
 /*Tim is an absolutely obsessive soda drinker, he simply cannot get enough. Most annoyingly though, he almost never has any money, so his only obvious legal way to obtain more soda is to take the money he gets when he recycles empty soda bottles to buy new ones. In addition to the empty bottles resulting from his own consumption he sometimes find empty bottles in the street. One day he was extra thirsty, so he actually drank sodas until he couldn’t afford a new one.
-
+TODO
 Input
 Three non-negative integers 
 , where 
