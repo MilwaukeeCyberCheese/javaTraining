6e12b450433ba01b2b4a9876c5d81cf389f01b60
Sam948-byte <techguy763@gmail.com>
2023-11-04 22:54:12 -0500

    add input for problem4

--- Problem4.java
+++ Problem4.java
@@ -6,23 +6,30 @@
  * the number of cities she has visited at least once.
  * 
  * Input
- * The first line of input contains a single positive integer indicating the
+ * The first line of input contains a single positive integer T <= 50 indicating the
  * number of test cases. The first line of each test case also contains a single
  * positive integer indicating the number of work trips Alice has taken so far.
- * The following lines describe these trips. The th such line simply contains
- * the name of the city Alice visited on her th trip.
+ * The following lines describe these trips. The nth such line simply contains
+ * the name of the city Alice visited on her nth trip.
  * 
  * Alice’s work only sends her to cities with simple names: city names only
  * contain lowercase letters, have at least one letter, and do not contain
  * spaces.
  * 
- * The number of trips is at most and no city name contains more than
+ * The number of trips is at most 100 and no city name contains more than 20
  * characters.
  * 
  * Output
  * For each test case, simply output a single line containing a single integer
  * that is the number of distinct cities that Alice has visited on her work
  * trips.
+ */
+
+/*
+ * Hints:
+ * 
+ * 1. Use a linked list to keep track of the cities Alice has visited so far.
+ * 2. Use the Scanner class to read the input.
  */
 
  public class Problem4{
---
+++ Problem5.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/*
+ * Given a base b and two non-negative base-b integers p and m, compute p mod m
+ * and print the result as a base b integer.
+ * p mod m is defined as the smallest non-negative integer k such that p = am + k for some integer a.
+ * 
+ * Input
+ * Input consists of a number of cases (at most 1000). Each case is represented
+ * by a line containing three non-negative integers. 
+ * The first, b, is a base-10 number between 2 and 10. 
+ * The second, p, contains up to 1000 digits between 0 and b-1. 
+ * The third, m, contains up to 9 digits between 0 and b-1. 
+ * The last case is followed by a line containing 0.
+ * 
+ * Output
+ * For each test case, print a line giving p mod m
+ * as a base-b
+ * integer.
+ */
+
+ /*
+  * HINTS:
+    * 1. You can use the BigInteger class to represent arbitrarily large integers.
+    * 2. You can use the Scanner class to read input from a file.
+  */
+ public class Problem5{
+    public static void main(String[] args) {
+        
+    }
+ }
