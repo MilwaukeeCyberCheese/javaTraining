a955ee99d73a7479e1fef2126e6e718526559b66
MilwaukeeCyberCheese <mkecybercheese@gmail.com>
2023-11-08 18:42:05 -0600

    remove extra stuff

--- Student.java
+++ Student.java
@@ -11,31 +11,9 @@ public class Student {
     private int test3;
 
     // Write your Class Constructor Here
-    public Student(String name, int test1, int test2, int test3) {
-        this.name = name;
-        this.test1 = test1;
-        this.test2 = test2;
-        this.test3 = test3;
-    }
+   
 
     // Write your Methods here
-    public char average() {
-        char letterGrade = 'E';
-        int average = Math.round((this.test1 + this.test2 + this.test3) / 3);
-
-        if ((90 <= average) && (average <= 100)) {
-            letterGrade = 'A';
-        } else if ((80 <= average) && (average < 90)) {
-            letterGrade = 'B';
-        } else if ((70 <= average) && (average < 80)) {
-            letterGrade = 'C';
-        } else if ((60 <= average) && (average < 70)) {
-            letterGrade = 'D';
-        } else { // Average < 60
-            letterGrade = 'F';
-        }
-
-        return letterGrade;
-    }
+   
 
 }
