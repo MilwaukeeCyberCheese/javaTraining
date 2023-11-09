5c4dc0ee5b675a3e4428440b12877af2609847e4
mkecybercheese <mkecybercheese@gmail.com>
2023-11-08 18:45:22 -0600

    remove class variables p2

--- Student.java
+++ Student.java
@@ -5,10 +5,7 @@
 public class Student {
     
     // Fill in your Class Variables here
-    public String name;
-    private int test1;
-    private int test2;
-    private int test3;
+  
 
     // Write your Class Constructor Here
    
